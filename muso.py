64df0533abd6deb74df30415923590df2c00f042
Duncan Lock <duncan.lock@gmail.com>
2013-04-09 18:19:11 -0700

    Added some placeholder funations to check folders and files for correctness - need to implement.

--- muso.py
+++ muso.py
@@ -26,6 +26,39 @@ def build_file_tree(root):
     return tree
 
 
+def check_artist_folder(path):
+    """
+    Check that the given path looks like an Artist folder should:
+
+    - has a folder.jpg
+    - otherwise, only contains folders, which contain music
+    """
+    pass
+
+
+def check_album_folder(path):
+    """
+    Check that the given path looks like an Album folder should:
+
+    - has a folder.jpg
+    - otherwise, only contains music
+    """
+    pass
+
+
+def check_music_file(file):
+    """
+    Check that the given path looks like a music file should:
+
+    - is an mp3|flac|ogg file
+    - filename matches one of these:
+        - Compilation Albums: <album> - <discnumber>.<tracknumber> - <artist> - <title>.ext
+        - Regular Albums:     <artist> - <album> - <discnumber>.<tracknumber> - <title>.ext
+    - Check tags
+    """
+    pass
+
+
 music = build_file_tree(os.path.expanduser('~') + '/Music')
 
 for artist in music.keys():
